0b35d5bddc106cc049ad7bb547957d93b9649b2d
tauqeer-trailfive <108520807+tauqeer-trailfive@users.noreply.github.com>
2023-11-10 16:23:38 +0500

    Added forn and success page

---
+++ src/Routes/Navigation.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Board from "../Screens/Board";
+import App from "../App";
+import Success from "../Screens/Success";
+
+type Props = {};
+
+export default function Navigation({}: Props) {
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/success" element={<Success />} />
+    </Routes>
+  );
+}
--- src/Screens/Board.tsx
+++ src/Screens/Board.tsx
@@ -1,3 +1,7 @@
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Link } from "react-router-dom";
+
 type BoardProps = {
   jobTitle: string;
   teaser?: string;
@@ -8,6 +12,206 @@ type BoardProps = {
 };
 
 const Board = (props: BoardProps) => {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function FormModal() {
+    return (
+      <>
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={closeModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black/25" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <Dialog.Panel className="w-full max-w-7xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                    <section className="bg-white dark:bg-gray-900 rounded-md">
+                      <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+                        <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+                          Fill the form
+                        </h2>
+                        <form action="#">
+                          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                            <div className="sm:col-span-2">
+                              <label
+                                htmlFor="name"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                First Name
+                              </label>
+                              <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="Full Name"
+                              />
+                            </div>
+                            <div className="w-full">
+                              <label
+                                htmlFor="email"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                Email
+                              </label>
+                              <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="Email Address"
+                              />
+                            </div>
+                            <div className="w-full">
+                              <label
+                                htmlFor="phone"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                Phone Number
+                              </label>
+                              <input
+                                type="number"
+                                name="phone"
+                                id="phone"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="Phone Number"
+                              />
+                            </div>
+                            <div>
+                              <label
+                                htmlFor="gender"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                Gender
+                              </label>
+                              <select
+                                id="gender"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                              >
+                                <option selected={true}>Select gender</option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                              </select>
+                            </div>
+                            <div>
+                              <label
+                                htmlFor="Motivation"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                Motivation
+                              </label>
+                              <input
+                                type="text"
+                                name="Motivation"
+                                id="Motivation"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="Motivation"
+                              />
+                            </div>
+                            <div className="sm:col-span-2">
+                              <label
+                                htmlFor="description"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                              >
+                                Description
+                              </label>
+                              <textarea
+                                id="description"
+                                rows={8}
+                                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                placeholder="Your description here"
+                              ></textarea>
+                            </div>
+                          </div>
+
+                          <div className="flex items-center justify-center w-full my-4">
+                            <label
+                              htmlFor="dropzone-file"
+                              className="flex flex-col items-center justify-center w-full h-32 border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                            >
+                              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                <svg
+                                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                                  aria-hidden="true"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 20 16"
+                                >
+                                  <path
+                                    stroke="currentColor"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                                  />
+                                </svg>
+                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                                  <span className="font-semibold">
+                                    Click to upload
+                                  </span>{" "}
+                                  or drag and drop
+                                </p>
+                                <p className="text-xs text-gray-500 dark:text-gray-400">
+                                  SVG, PNG, JPG or GIF (MAX. 800x400px)
+                                </p>
+                              </div>
+                              <input
+                                id="dropzone-file"
+                                type="file"
+                                className="hidden"
+                              />
+                            </label>
+                          </div>
+
+                          <div className="mt-4">
+                            <Link
+                              to={"/success"}
+                              type="button"
+                              className="inline-flex justify-center rounded-md border w-full border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                              onClick={closeModal}
+                            >
+                              Submit Now
+                            </Link>
+                          </div>
+                        </form>
+                      </div>
+                    </section>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
+      </>
+    );
+  }
+
   return (
     <section className="bg-white dark:bg-gray-900 max-w-2xl px-10 rounded-xl shadow-2xl my-2">
       <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
@@ -44,10 +248,13 @@ const Board = (props: BoardProps) => {
           </div>
         </dl>
         <div className="flex items-center space-x-4">
-          <a
-            href={props.ShortUrl}
+          <button
+            // href={props.ShortUrl}
+            onClick={() => {
+              setIsOpen(true);
+            }}
             type="button"
-            target="_blank"
+            // target="_blank"
             className="inline-flex items-center text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
           >
             <svg
@@ -65,9 +272,10 @@ const Board = (props: BoardProps) => {
               />
             </svg>
             Apply
-          </a>
+          </button>
         </div>
       </div>
+      <FormModal />
     </section>
   );
 };
