50652d2795269ce12bc6756740dd962ad35d85d1
Tauqeer Nasir <108520807+tauqeer-trailfive@users.noreply.github.com>
2023-12-18 11:25:53 +0500

    Added Apply Form

--- src/Routes/Navigation.tsx
+++ src/Routes/Navigation.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Board from "../Screens/Board";
 import App from "../App";
 import Success from "../Screens/Success";
+import ApplyForm from "../Screens/ApplyForm";
 
 type Props = {};
 
@@ -10,6 +11,7 @@ export default function Navigation({}: Props) {
   return (
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/apply" element={<ApplyForm />} />
       {/* <Route path="/success" element={<Success />}  /> */}
     </Routes>
   );
--- src/Screens/Board.tsx
+++ src/Screens/Board.tsx
@@ -248,7 +248,7 @@ const Board = (props: BoardProps) => {
           </div>
         </dl>
         <div className="flex items-center space-x-4">
-          <a
+          {/* <a
             id="apply-button"
             href={props.ShortUrl}
             // onClick={() => {
@@ -276,7 +276,36 @@ const Board = (props: BoardProps) => {
                 clipRule="evenodd"
               />
             </svg>
-          </a>
+          </a> */}
+          <Link
+            id="apply-button"
+            to={"/apply"}
+            // onClick={() => {
+            //   setIsOpen(true);
+            // }}
+            type="button"
+            // target="_blank"
+            className="inline-flex items-center justify-center text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium normal-case rounded-lg text-md px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
+          >
+            Apply
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-4 h-4 ml-2"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z"
+                clipRule="evenodd"
+              />
+              <path
+                fillRule="evenodd"
+                d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Link>
         </div>
       </div>
       <FormModal />
