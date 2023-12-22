c991faed6939506c5a1a51d0b56421d6d429fdfd
ismail butt <ismail.butt@trailfive.com>
2023-12-22 20:18:43 +0500

    Added id for button

--- src/Screens/ApplyForm.tsx
+++ src/Screens/ApplyForm.tsx
@@ -285,6 +285,7 @@ export default function Client({}: Props) {
             </Link> */}
             <button
               type="button"
+              id="Submit-Now"
               className="inline-flex justify-center rounded-md border w-full border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
               onClick={handleSubmit}
             >
