89588d5279c7cc98c9911d1e368046e4f33b1b1f
ismail butt <ismail.butt@trailfive.com>
2023-12-22 20:20:00 +0500

    Added id for button

--- src/Screens/ApplyForm.tsx
+++ src/Screens/ApplyForm.tsx
@@ -85,6 +85,7 @@ export default function Client({}: Props) {
                 First Name
               </label>
               <input
+                id="name"
                 type="text"
                 className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 
                 ${
