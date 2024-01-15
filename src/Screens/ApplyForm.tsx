936228e7ac889945669203cfad52c0411a2d2e6d
ismail butt <ismail.butt@trailfive.com>
2024-01-15 19:31:06 +0500

    gtm tag manager

--- src/Screens/ApplyForm.tsx
+++ src/Screens/ApplyForm.tsx
@@ -118,24 +118,8 @@ export default function Client({ }: Props) {
 
   return (
     <section className="bg-white dark:bg-gray-900 min-h-screen">
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-NXGNXGL6"
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        ></iframe>
-      </noscript>
-      {/* <!-- Google Tag Manager (noscript) --> Moiz*/}
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-52CF7Q6R"
-          height="0"
-          width="0"
-          style={{ visibility: "hidden", display: "none" }}
-        ></iframe>
-      </noscript>
-      {/* <!-- End Google Tag Manager (noscript) --> Moiz*/}
+
+
       <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
         <button
           onClick={() => navigate(-1)}
